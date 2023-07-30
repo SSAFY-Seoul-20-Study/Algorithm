5f82f17ef0404ad1cf718787faab395b87dc0a77
unknown <xotpqnd@gmail.com>
2023-07-30 10:41:26 +0900

    8월 1주차 과제 완료

--- 한태희/Backjoon_17136_색종이_붙히기/Backjoon_17136_색종이_붙히기.java
+++ 한태희/Backjoon_17136_색종이_붙히기/Backjoon_17136_색종이_붙히기.java
@@ -1,10 +1,19 @@
 import java.util.*;
 import java.io.*;
 
+//인터넷 예제를 참조했습니다.
+//출처:
+//https://velog.io/@ohjinseo/%EB%B0%B1%EC%A4%80-17136%EB%B2%88-%EC%83%89%EC%A2%85%EC%9D%B4-%EB%B6%99%EC%9D%B4%EA%B8%B0-C
+
 public class Backjoon_17136_색종이_붙히기
 {
+	static int N=10; //배열 크기
+	static int M=5; //색총이의 최대 크기 (1~5)
+	static int K=5; //색종이의 개수
+	static int min = 999999999;
+
 	static int[][] arr;
-	static int N=10;
+	static int[] paperLeft;
 
 	public static void main(String args[]) throws Exception
 	{
@@ -21,39 +30,50 @@ public class Backjoon_17136_색종이_붙히기
 			}
 		}
 
-		//for(int [] b : arr) System.out.println(Arrays.toString(b));
-		int paperUseCnt = 0;
-		int MAX_PAPER = 5;
-		int[] psizelist = {5, 4, 3, 2, 1};
-		papersize:for(int psize:psizelist){
-			int left_p = MAX_PAPER;
+		paperLeft = new int[M+1];
+		for(int m=1;m<=M;m++)
+			paperLeft[m] = K;
 
-			for(int i=0;i<=N-psize;i++){
-				for(int j=0;j<=N-psize;j++){
-					if(paperCanAttatched(i,j,psize)){
-						attachPaper(i,j,psize);
-						paperUseCnt ++;
-						left_p --;
-						if(left_p ==0) continue papersize;
+		recursive_attach(0);
+		if(min ==999999999)
+			min = -1;
+		System.out.println(min);
+		br.close();
+	}
+
+	static void recursive_attach(int depth){
+		if(depth > min) return;
+
+		if(coveredAll() && min > depth) min = depth;
+
+		for(int i=0;i<N;i++){
+			for(int j=0;j<N;j++){
+				if(arr[i][j] ==1){
+					for(int m=5;m>=1;m--){
+						if(paperLeft[m]>0 && paperCanAttatched(i, j, m)){
+							paperLeft[m] --;
+							attachPaper(i,j,m);
+
+							recursive_attach(depth+1);
+							//재귀 호출 이후 원상복구
+							paperLeft[m] ++;
+							detachPaper(i,j,m);
+
+						}
 					}
+
+					return;
 				}
 			}
 		}
-		if(!CoveredAll())
-			paperUseCnt = -1;
-
-		System.out.println(paperUseCnt);
-
-		br.close();
 	}
 
 	private static boolean paperCanAttatched(int row, int col, int psize) {
 		for(int i=row;i<row+psize;i++)
 			for(int j=col;j<col+psize;j++)
-				if(arr[i][j] ==0)
+				if(!isValid(i, j) || arr[i][j] ==0)
 					return false;
 			
-		
 		return true;
 	}
 
@@ -63,7 +83,13 @@ public class Backjoon_17136_색종이_붙히기
 				arr[i][j] = 0;
 	}
 
-	private static boolean CoveredAll() {
+		private static void detachPaper(int row, int col, int psize) {
+		for(int i=row;i<row+psize;i++)
+			for(int j=col;j<col+psize;j++)
+				arr[i][j] = 1;
+	}
+
+	private static boolean coveredAll() {
 		for(int i=0;i<N;i++)
 			for(int j=0;j<N;j++)
 				if(arr[i][j]==1)
@@ -72,4 +98,11 @@ public class Backjoon_17136_색종이_붙히기
 		return true;
 	}
 
+	
+	static boolean isValid(int row, int col){
+		if(0<=row && row<N && 0<=col && col <N)
+			return true;
+		else
+			return false;
+	}
 }
