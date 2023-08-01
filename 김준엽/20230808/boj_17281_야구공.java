263875b53e0f8423f3aa03432cc4ae6e2ed39bca
top1506 <top1506@naver.com>
2023-08-01 16:28:37 +0900

    배열돌리기 문제 풀이완료

---
+++ 김준엽/20230808/boj_17281_야구공.java
@@ -0,0 +1,12 @@
+package study;
+
+import java.util.*;
+import java.io.*;
+public class Main_17281 {
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("study/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
