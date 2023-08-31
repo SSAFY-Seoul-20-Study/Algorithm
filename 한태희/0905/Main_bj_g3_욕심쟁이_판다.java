8c60ece53c55ca5e0c11621e41905d9e1d023b63
unknown <xotpqnd@gmail.com>
2023-08-31 17:16:55 +0900

    욕심쟁이 판다 완료

--- 한태희/0905/Main_bj_g3_욕심쟁이_판다.java
+++ 한태희/0905/Main_bj_g3_욕심쟁이_판다.java
@@ -8,10 +8,7 @@ public class Main_bj_g3_욕심쟁이_판다 {
 	static int[] dr = { 0, 1, 0, -1 };
 	static int[] dc = { 1, 0, -1, 0 };
 
-	static int max_depth;
-
 	public static void main(String args[]) throws Exception {
-		System.setIn(new FileInputStream("한태희/res/panda.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
@@ -24,53 +21,34 @@ public class Main_bj_g3_욕심쟁이_판다 {
 			}
 		}
 
-		List<int[]> SP = new ArrayList<>();
+		int max_depth = 0;
+		b = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				int fanIn = 0;
-				for (int k = 0; k < 4; k++) {
-					int r = i + dr[k];
-					int c = j + dc[k];
-					if (isValid(r, c)) {
-						if (a[r][c] < a[i][j]) {
-							fanIn++;
-						}
-					}
-				}
-				if (fanIn == 0) {
-					SP.add(new int[] { i, j });
-				}
+				max_depth = Integer.max(max_depth, dfs(i, j));
 			}
 		}
 
-		max_depth = 0;
-		b = new int[N][N];
-		for (int[] sp : SP) {
-			b[sp[0]][sp[1]] = 1;
-			dfs(sp[0], sp[1], 1);
-		}
-
-		System.out.println(max_depth);
+		System.out.print(max_depth);
 
 		br.close();
 	}
 
-	static void dfs(int r, int c, int depth) {
-		int fanOut = 0;
-		for (int k = 0; k < 4; k++) {
-			int nr = r + dr[k];
-			int nc = c + dc[k];
-			if (isValid(nr, nc) == false)
-				continue;
-			if (a[nr][nc] > a[r][c] && depth + 1 > b[nr][nc]) {
-				b[nr][nc] = depth + 1;
-				fanOut++;
-				dfs(nr, nc, depth + 1);
+	static int dfs(int r, int c) {
+
+		if (b[r][c] == 0) {
+			int local_max = 0;
+			for (int k = 0; k < 4; k++) {
+				int nr = r + dr[k];
+				int nc = c + dc[k];
+				if (isValid(nr, nc) && a[r][c] < a[nr][nc]) {
+					local_max = Integer.max(local_max, dfs(nr, nc));
+				}
 			}
+			b[r][c] = 1 + local_max;
 		}
-		if (fanOut == 0) {
-			max_depth = Math.max(depth, max_depth);
-		}
+
+		return b[r][c];
 	}
 
 	static boolean isValid(int r, int c) {
