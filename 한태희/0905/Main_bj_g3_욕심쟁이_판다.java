d5ef7b501cca46327ae1f4a95840b0d3a4616d77
unknown <xotpqnd@gmail.com>
2023-09-01 11:21:44 +0900

    완료

--- 한태희/0905/Main_bj_g3_욕심쟁이_판다.java
+++ 한태희/0905/Main_bj_g3_욕심쟁이_판다.java
@@ -1,6 +1,16 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * DP + DFS 를 이용한 아이디어.
+ * 임의의 위치 k와, k의 상하좌우 위치가 있을 때,
+ * k위치에서 판다가 생존 가능한 최대 시간은
+ * ( (상하좌우 위치 중 k보다 값이 큰 위치의) 생존 가능한 최대 시간 중)  최댓값
+ * 임을 알 수 있다.
+ * 
+ * 2차원 배열의 각 점에서 생존 가능한 최대 시간을 메모이제이션 한다면,
+ * 위의 점화식을 빠르게 구할 수 있다.
+ */
 public class Main_bj_g3_욕심쟁이_판다 {
 	static int N;
 	static int[][] a, b;
@@ -9,10 +19,9 @@ public class Main_bj_g3_욕심쟁이_판다 {
 	static int[] dc = { 1, 0, -1, 0 };
 
 	public static void main(String args[]) throws Exception {
+		//N의 크기와 각 위치의 대나무의 개수 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		N = Integer.parseInt(br.readLine());
-
 		a = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,6 +30,7 @@ public class Main_bj_g3_욕심쟁이_판다 {
 			}
 		}
 
+		//모든 점을 순회하면서 판다가 생존 가능한 최댓값을 구한다.
 		int max_depth = 0;
 		b = new int[N][N];
 		for (int i = 0; i < N; i++) {
@@ -34,6 +44,13 @@ public class Main_bj_g3_욕심쟁이_판다 {
 		br.close();
 	}
 
+	/**
+	 * 만약 b 배열 (메모이제이션 배열)이 0이 아닌 값이라면,
+	 * 그 값이 해당 위치에서의 판다의 최대 생존 시간임으로 바로 리턴한다.
+	 * 아직 b 배열이 초기 상태라면, 탐색이 필요함으로
+	 * 4방향을 탐색해서 현재 위치보다 대나무가 더 많은 위치의 
+	 * 판다의 생존 시간 중 가장 값이 큰 위치를 찾는다.
+	 */
 	static int dfs(int r, int c) {
 
 		if (b[r][c] == 0) {
--- 한태희/0905/Main_bj_g5_맥주_마시면서_걸어가기.java
+++ 한태희/0905/Main_bj_g5_맥주_마시면서_걸어가기.java
@@ -1,6 +1,18 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * 유니온 파인드 문제.
+ * 맥주를 20병을 들고 50칸을 걸을때마다 1병씩 소모한다는 것은,
+ * 1000칸은 추가적인 보급 없이 걸어갈 수 있다는 것이다.
+ * 
+ * 따라서 두 편의점 사이의 거리가 1000 이하이라면, 두 편의점 사이는 이동 가능한 것이고
+ * 1000을 초과한다면 이동 불가능하다.
+ * 이걸 그래프 속 노드 두개의 연결이 되어있다, 연결이 없다로 생각할 수 있다.
+ * 
+ * 시작점부터 유니온 파인드를 하면서 집과 축제장이 같은 그래프 안에
+ * 연결돼있는지 찾으면 되는 문제이다.
+ */
 public class Main_bj_g5_맥주_마시면서_걸어가기 {
 	static int[] dr = { 0, 1, 0, -1 };
 	static int[] dc = { 1, 0, -1, 0 };
@@ -11,9 +23,14 @@ public class Main_bj_g5_맥주_마시면서_걸어가기 {
 
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
+		testcase: for (int tc = 1; tc <= T; tc++) {
+
+			// 정보 입력
+			// 정수 3개가 들어있는 cord 배열을 사용한다.
+			// 0:row 1:col 2:집에서의 거리 를 의미한다.
 			N = Integer.parseInt(br.readLine());
 
 			int[] startC = new int[3];
@@ -37,19 +54,22 @@ public class Main_bj_g5_맥주_마시면서_걸어가기 {
 			endC[1] = Integer.parseInt(st.nextToken());
 			endC[2] = INF;
 
+			// BFS를 이용해서 그래프를 탐색한다.
 			Queue<int[]> q = new ArrayDeque<>();
 			q.offer(startC);
 
 			while (q.isEmpty() == false) {
 				int[] cord = q.poll();
-				// System.out.println(Arrays.toString(cord));
 
 				if (getDist(cord, endC) <= 1000) {
-					System.out.println("happy");
-					return;
+					sb.append("happy\n");
+					continue testcase;
 				}
 
 				for (int[] convC : convC_arr) {
+					// 만약 그래프가 연결되어 있지 않거나,
+					// 다음 좌표의 집과의 거리가 현재 좌표의 집과의 거리보다 작다면
+					// 탐색할 필요가 없다.
 					if (getDist(cord, convC) <= 1000 && convC[2] > cord[2]) {
 						convC[2] = cord[2] + 1;
 						q.offer(convC);
@@ -57,8 +77,10 @@ public class Main_bj_g5_맥주_마시면서_걸어가기 {
 				}
 			}
 
-			System.out.println("sad");
+			sb.append("sad\n");
 		}
+
+		System.out.print(sb);
 
 		br.close();
 	}
