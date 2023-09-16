327d46653b0f289fca2b3e14cc598fa3b04d1435
unknown <xotpqnd@gmail.com>
2023-09-16 12:32:50 +0900

    문제풀이 완료, 주석을 아직 안담

--- 한태희/0919/Main_bj_g3_1939_중량_제한.java
+++ 한태희/0919/Main_bj_g3_1939_중량_제한.java
@@ -3,60 +3,90 @@ import java.io.*;
 
 public class Main_bj_g3_1939_중량_제한 {
 	static int N, M;
-	static int[] a;// 최대 유량
-	static List<int[]>[] g;// 그래프 연결 정보
+	static int start, end;
+	static ArrayList<int[]>[] list;// 그래프 연결 정보
+	static boolean[] v;
 
 	final static int INF = Integer.MAX_VALUE;
 
+	static int ans;
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		a = new int[N + 1];
-		g = new List[N + 1];
-		for (int i = 1; i < N + 1; i++) {
-			g[i] = new ArrayList<>();
-		}
+		list = new ArrayList[N + 1];
+
+		int left=INF;
+		int right=0;
 
 		for (int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int z = Integer.parseInt(st.nextToken());
-			g[a].add(new int[] { b, z });
-			g[b].add(new int[] { a, z });
+			left = Math.min(left, z);
+			right = Math.max(right, z);
+
+			if(list[a]==null) list[a] = new ArrayList<>();
+			list[a].add(new int[] {b, z});
+			if(list[b]==null) list[b] = new ArrayList<>();
+			list[b].add(new int[] {a, z});
 		}
 
 		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
+		start = Integer.parseInt(st.nextToken());
+		end = Integer.parseInt(st.nextToken());
+		ans = 0;
 
-		Queue<int[]> q = new ArrayDeque<>(); // {선택 노드, 현재 유량}
-		q.offer(new int[] { start, INF });
-		a[start] = INF;
+		while(left <= right){
+			int mid = (left + right)/2;
+			v = new boolean[N + 1];
+			if(check(mid)){
+				left = mid + 1;
+				ans = mid;
+			}
+			else{
+				right = mid - 1;
+			}
+		}
 
-		while (q.isEmpty() == false) {
-			int[] info = q.poll();
-			int now = info[0];
-			int flow = info[1];
+		System.out.println(ans);
 
-			for (int[] edge : g[now]) {
-				int next = edge[0];
-				int weight = edge[1];
-				int bottleneck = Math.min(flow, weight);
-				if (a[next] < bottleneck) {
-					a[next] = bottleneck;
-					q.offer(new int[] { next, bottleneck });
+		br.close();
+	}
+
+	/**
+	 * 크기가 k 이상인 다리들만 사용하여 start에서 end로 건너갈 수 있는지 확인한다.
+	 */
+	static boolean check(int k){
+		v = new boolean[N + 1];
+		v[start] = true;
+		Queue<Integer> q = new ArrayDeque<>();
+		q.offer(start);
+		
+		while(q.isEmpty()==false){
+			int now = q.poll();
+			ArrayList<int[]> edges = list[now];
+
+			if(edges==null) continue;
+
+			for(int info[]: edges){
+				int next = info[0];
+				int weight = info[1];
+
+				if(v[next]==false && weight >= k){
+					if(next==end){
+						return true;
+					}
+					q.offer(next);
+					v[next] = true;
 				}
 			}
 		}
 
-		int ans = a[end];
-		System.out.println(ans);
-
-		br.close();
+		return false;
 	}
 
 }
