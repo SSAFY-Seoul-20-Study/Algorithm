33f6794949c80571371bccbaa143a2ac3f0df7bf
한태희 <xotpqnd@gmail.com>
2024-02-19 13:10:30 +0900

    한태희 0220 3문제 완료

--- 한태희/0220/Main_bj_g3_14658_하늘에서_별똥별이_빗발친다.java
+++ 한태희/0220/Main_bj_g3_14658_하늘에서_별똥별이_빗발친다.java
@@ -2,129 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main_bj_g3_14658_하늘에서_별똥별이_빗발친다 {
-	static int N, M;
-	static boolean[][] obst;
-	static boolean[][] v;
 
-	static int targetVisit, answer;
-
-	static int[] dr = {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
+	static int N, M, L, K;
 
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-		int tc =1;
-		String input="";
-
-		while((input = br.readLine())!= null){
-			StringTokenizer st = new StringTokenizer(input);
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-
-			obst = new boolean[N][M];
-			targetVisit = 0;
-			
-			for (int i = 0; i < N; i++) {
-				String s = br.readLine();
-				for (int j = 0; j < M; j++) {
-					if(s.charAt(j)=='*'){
-						obst[i][j] = true;
-					}
-					if(s.charAt(j)=='.'){
-						obst[i][j] = false;
-						targetVisit +=1;
-					}
-				}
-			}
-			v = new boolean[N][M];
-			answer = Integer.MAX_VALUE;
-
-			if(targetVisit ==1){
-				answer = 0;
-			}
-			else{
-				for(int i=0; i<N; i++){
-					for(int j=0; j<M; j++){
-						if(obst[i][j] == false){
-							for(int k=0; k<4; k++){
-								int ni = i + dr[k];
-								int nj = j + dc[k];
-								if(isValid(ni, nj) == false){
-									continue;
-								}
-	
-								v[i][j] = true;
-								dfs(i, j, k, 1, 1);
-								v[i][j] = false;
-							}
-						}
-					}
-				}
-			}
-
-			sb.append("Case ").append(tc++).append(": ");
-
-			if(answer == Integer.MAX_VALUE){
-				sb.append(-1);
-			}
-			else{
-				sb.append(answer);
-			}
-
-			sb.append("\n");
-
+		int[][] cords = new int[K][];
+		Set<Integer> xSet = new HashSet<>();
+		Set<Integer> ySet = new HashSet<>();
+		for(int k=0;k<K;k++) {
+			st = new StringTokenizer(br.readLine());
+			cords[k] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+			xSet.add(cords[k][0]);
+			ySet.add(cords[k][1]);
 		}
 
-		System.out.println(sb);
+		int answer = Integer.MAX_VALUE;
+
+		for(int x: xSet){
+			for(int y: ySet){
+				int leftStars = K;
+				for(int k=0;k<K;k++){
+					if(x <= cords[k][0] && cords[k][0] <= x + L && y <= cords[k][1] && cords[k][1] <= y + L){
+						leftStars--;
+					}
+				}
+				answer = Math.min(answer, leftStars);
+			}
+		}
+
+		System.out.println(answer);
 
 		br.close();
 	}
 
-	static void dfs(int r, int c, int dir, int level, int visits){
-		if(visits == targetVisit && level < answer){
-			answer = level;
-			return;
-		}
-
-		if(level > answer){
-			return;
-		}
-
-		int nr = r + dr[dir];
-		int nc = c + dc[dir];
-
-		if(isValid(nr, nc)){
-
-			v[nr][nc] = true;
-			dfs(nr, nc, dir, level, visits+1);
-			v[nr][nc] = false;
-
-		}
-		else{
-
-			for(int k=0;k<4;k++){
-				if(k==dir) continue;
-				nr = r + dr[k];
-				nc = c + dc[k];
-
-				if(isValid(nr, nc)){
-					v[nr][nc] = true;
-					dfs(nr, nc, k, level+1, visits+1);
-					v[nr][nc] = false;
-				}
-			}
-
-		}
-
-	}
-
-	static boolean isValid(int r, int c){
-		if(0<=r && r<N && 0<=c && c<M && obst[r][c] == false && v[r][c] == false){
-			return true;
-		}else{
-			return false;
-		}
-	}
 }
