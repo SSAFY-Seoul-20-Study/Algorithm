e1ae86444e2a922d95046f4f42122a57dbc40f13
한태희 <xotpqnd@gmail.com>
2024-02-21 17:55:58 +0900

    큐빙 문제 풀이중

---
+++ 한태희/0227/Main_bj_p5_5373_큐빙.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_bj_p5_5373_큐빙 {
+
+	public static void main(String args[]) throws Exception {
+		
+		char[][] top, bottom, front, back, left, right;
+		top = 
+
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.close();
+	}
+}
